ffa59d39344fb972346eeb096596f0c86cbd3b09
alacherrygruszkapietruszka <ala.7245@wp.pl>
2024-05-10 13:02:51 +0200

    wspólne robienie projektów

--- skrypt.py
+++ skrypt.py
@@ -1,6 +1,8 @@
 from math import sin, cos, sqrt, atan, atan2, degrees, radians
 import numpy as np
 import argparse as args, argparse
+from argparse import ArgumentParser
+
 
 o = object()
 
@@ -238,8 +240,72 @@ class Transformacje:
                     result.append([x92, y92])
                    
                     return result 
+             
+        def file_save92(self, X, Y, Z, file_out):
+                """
+                Funkcja ta pozwala zapisać współrzędne otrzymane po transformacji XYZ -> PL-1992 
+                do pliku wyjsciowego file_out w formacie .txt
+                """
                 
-            def plik(self, file, transf):
+                X92 = []
+                Y92 = []
+                for a, b, c in zip(X, Y, Z):
+                    x92, y92 = Transformacje.PL1992(a, b, c)           
+                    X92.append(x92)
+                    Y92.append(y92)
+                    
+                plik=open(file_out,"w")
+                plik.write(f'# PL-1992---------------------------------------------- \n')
+                plik.write(f'  X[m]         Y[m] \n')
+                plik.write(f'# ----------------------------------------------------- \n')
+                for a,b in zip(X92,Y92):
+                    a = f'{a:7.3f}'
+                    b = f'{b:7.3f}'
+                    plik.write(f'{a},   {b} \n')
+                plik.close()
+ 
+           
+if __name__ == "_main_":
+    geo = Transformacje("GRS80")
+    geo.wczytanie_zapisanie_pliku("wsp_inp.txt")
+    
+    parser = ArgumentParser()
+    parser.add_argument('-m', '--m', type=str, help="Podaj jedną z wskazanych elipsoid: GRS80, WGS84, Krasowski")
+    parser.add_argument('-neu', '--neu', type=str, help="Podaj nazwe pliku wynikiowego dla neu z rozszerzeniem txt")
+    parser.add_argument('-xa', '--xa', type=float)
+    parser.add_argument('-ya', '--ya', type=float)
+    parser.add_argument('-za', '--za', type=float)
+    parser.add_argument('-xb', '--xb', type=float)
+    parser.add_argument('-yb', '--yb', type=float)
+    parser.add_argument('-zb', '--zb', type=float)
+    args = parser.parse_args()
+ 
+ 
+    geo = Transformacje(model = args.m)
+    f, l, h = Transformacje.hirvonen(args.xa, args.ya, args.za)
+    n, e, u = Transformacje.xyz2neu(f, l, args.xa, args.ya, args.za, args.xb, args.yb, args.zb)
+     
+     
+    geo.zapis_w_kalkulatorze_neu(args.neu, n, e, u)
+     
+    n = float(n)
+    e = float(e)
+    u = float(u)
+     
+     
+    print("")
+    print("")
+    print("")
+    print("")
+    print("Elipsida:", args.m)
+    print(f"Wyniki_z_xyz2neu; n = {n}, e = {e}, u = {u}")
+    print("Nazwa pliku głównego:", skrypt.py.__name__)
+    print("")
+    print("")
+    print("")
+    print("")  
+                 
+'''            def plik(self, file, transf):
                 dane = np.genfromtxt(file,delimiter = ' ')
                 if transf == 'xyz2plh':
                     result  = self.xyz2plh(dane[:,0], dane[:,1], dane[:,2])
@@ -284,4 +350,4 @@ class Transformacje:
         except IndexError:
             print("Nieodpowiedni format pliku. Proszę wprowadzić dane do pliku tak jak pokazano w przyładzie.")
         except ValueError:
-            print("Nieodpowiedni format pliku. Proszę wprowadzić dane do pliku tak jak pokazano w przyładzie.")
+            print("Nieodpowiedni format pliku. Proszę wprowadzić dane do pliku tak jak pokazano w przyładzie.")'''
